cd9d5f6404569e1c7597ea861b1aedfa6d33ae57
Everton Constantino <everton.constantino@hotmail.com>
2020-12-10 12:30:24 +0000

    WIP

--- Eigen/src/Core/arch/NEON/MatrixProduct.h
+++ Eigen/src/Core/arch/NEON/MatrixProduct.h
@@ -1565,7 +1565,6 @@ EIGEN_STRONG_INLINE void gemm(const DataMapper& res, const Scalar* blockA, const
         const Scalar *lhs_base = blockA;
 
         Index row = 0;
-/*
         for(; row + 6*accCols <= rows; row += 6*accCols)
         {
 #define MICRO() \
@@ -1947,7 +1946,7 @@ EIGEN_STRONG_INLINE void gemm(const DataMapper& res, const Scalar* blockA, const
           res.template storePacketBlock<Packet, 4>(row + 1*accCols, col, acc2);
 #undef MICRO
         }
-*/
+
         for(; row + accCols <= rows; row += accCols)
         {
 #define MICRO() \
